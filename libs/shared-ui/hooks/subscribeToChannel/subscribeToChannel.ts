9cd114c7088b8eb1d068b675f453d609d7883cd3
LarsG <lars.gunnar.kodehodet@gmail.com>
2023-04-17 23:56:56 +0200

    some cleanup

--- libs/shared-ui/hooks/subscribeToChannel/subscribeToChannel.ts
+++ libs/shared-ui/hooks/subscribeToChannel/subscribeToChannel.ts
@@ -5,7 +5,7 @@ import {
 } from 'nats.ws'
 
 const codec = StringCodec()
-const CHANNEL_URLS = "ws://localhost:8080/"
+const CHANNEL_URLS = "ws://localhost:23422/"
 
 export interface ChannelOptions {
   subject: string,
